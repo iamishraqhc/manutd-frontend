67df31eb59d8e04abc02b8c954e4d381c1e636b7
Ishraq Haider Chowdhury <ishraq-haider.chowdhury@stud.uni-bamberg.de>
2021-08-18 17:02:04 +0200

    added position headlines

--- src/components/FetchData.js
+++ src/components/FetchData.js
@@ -4,7 +4,8 @@ import { Grid, Card, CardMedia, CardContent, Typography } from '@material-ui/cor
 export default class FetchData extends React.Component {
     state = {
       loading: true,
-      player: []
+      player: [],
+      position: []
     }
       
     async componentDidMount () {
@@ -14,10 +15,11 @@ export default class FetchData extends React.Component {
   
       const {
         players = [],
+        positionList = players.slice(0,4).map(item => item.position).flat(),
         memberList = players.slice(0,4).map(item => item.members).flat(),
       } = data
   
-      this.setState({player: memberList, loading: false})
+      this.setState({player: memberList, position: positionList, loading: false})
     }
     
     render() {
@@ -33,24 +35,33 @@ export default class FetchData extends React.Component {
       return (
           <div>
               <h1>Manchester United Players</h1>
-              <Grid container spacing={2} style={{paddingTop: "20px", paddingLeft: "50px", paddingRight: "50px"}}>
-                {this.state.player.map((item, key) => {
-                  return (
-                    
-                      <Grid item xs={3} key={key}>
-                        <Card>
-                          <CardMedia image={'https://' + item.image} style={{ width: "250px", height: "300px", margin: "auto" }} />
-                          <CardContent>
-                            <Typography><b>{item.name}</b></Typography>
-                            <Typography><b>Position:</b> {item.position}</Typography>
-                            <Typography><b>Jersey No:</b> {item.jersey}</Typography>
-                            <Typography><b>Country:</b> {item.country}</Typography>
-                          </CardContent>
-                        </Card>
-                      </Grid>
-                  )
-                })}
-              </Grid>
+              {this.state.position.map((place, index) => {
+                return (
+                  <Grid key={index}>
+                    <h1>{place}s</h1>
+                    <Grid container spacing={2} style={{paddingTop: "20px", paddingLeft: "50px", paddingRight: "50px"}}>
+                      {this.state.player.map((item, key) => {
+                        if (place === item.position) {
+                          return (
+                            <Grid item xs={3} key={key}>
+                              <Card>
+                                <CardMedia image={'https://' + item.image} style={{ width: "250px", height: "300px", margin: "auto" }} />
+                                <CardContent>
+                                  <Typography><b>{item.name}</b></Typography>
+                                  <Typography><b>Position:</b> {item.position}</Typography>
+                                  <Typography><b>Jersey No:</b> {item.jersey}</Typography>
+                                  <Typography><b>Country:</b> {item.country}</Typography>
+                                </CardContent>
+                              </Card>
+                            </Grid>
+                          )
+                        }
+                      })}
+                    </Grid>
+                  </Grid>
+                )
+              })}
+              
           </div>
       )
     }
